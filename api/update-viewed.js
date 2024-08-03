ccf8c2b56e59b8cea7d53329f8f501b82d54600b
gntrazios <t8190124@aueb.gr>
2024-08-03 20:24:10 +0300

    Initialize the data.json and add auto-increment of column Viewed

--- api/update-viewed.js
+++ api/update-viewed.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     // Find the selected cocktail and update the viewed status
     const selectedCocktail = jsonData.find(cocktail => cocktail.name === name);
     if (selectedCocktail) {
-      selectedCocktail.viewed = true; // Update viewed status or other properties as needed
+        selectedCocktail.Viewed += 1;
     } else {
       return res.status(404).json({ error: 'Cocktail not found' });
     }
